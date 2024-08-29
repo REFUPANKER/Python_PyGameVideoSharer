0a47d14144be8399554ce9226af51122ab7f67af
REFUPANKER <68808212+REFUPANKER@users.noreply.github.com>
2024-08-29 15:53:51 +0300

    fixed systems for latest ui update of insta

--- uploader.py
+++ uploader.py
@@ -1,3 +1,4 @@
+import pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -9,179 +10,169 @@ import os
 import json
 
 
-def ClickToButtonNow(driver, xpath):
-    button = driver.find_element(By.XPATH, xpath)
-    button.click()
-
+def TryToClickButton(driver,until,xpath):
+    try:
+        ClickToButton(driver,until,xpath)
+    except:
+        print("Cant click to")
+        print(xpath)
 
 def ClickToButton(driver, until, xpath):
     button = WebDriverWait(driver, until).until(
         EC.presence_of_element_located((By.XPATH, xpath)))
     button.click()
 
-
 def SendKeys(driver, until, sendkeys, xpath):
-    button = WebDriverWait(driver, until).until(
+    element = WebDriverWait(driver, until).until(
         EC.presence_of_element_located((By.XPATH, xpath)))
-    button.send_keys(sendkeys)
+    element.send_keys(sendkeys)
 
+def SaveCookies(driver):
+    cookies = driver.get_cookies()
+    print("Saving auth cookies...")
+    with open("cookies.pkl", "wb") as file:
+        pickle.dump(cookies, file)
+    print("Cookies saved")
+
+def FillCookieManual(driver):
+    print("Receiving cookie data...")
+    while input("Type 'ready' to finish manual filling: ") != "ready":
+        pass
+    SaveCookies(driver)
 
 def Run():
     print("Uploader started")
 
-    # Instagram bilgilerinizi girin
-    # uses credential.json
+    # Instagram credentials
     with open(".credential", "r") as file:
         credential = json.load(file)
-        """
-        {"email":"","password":""}
-        """
     username = credential["email"]
     password = credential["password"]
-
-    # get account info from environment 
-    #username = os.environ["email"]
-    #password = os.environ["password"]
-
 
     image_path = os.path.abspath("ballbounce.mp4")
     content_size = os.path.getsize(image_path)
     content_uploadSize = int(content_size / (1024 * 1024)) + 1
-    # multiline description > [!] includes pre-spaces
     post_description = """its damn problem! here is the info about big planes...
 #software #python #pygame #fyp #instagram #game #adhd
 #explore #colors #rgb #gaming #gamer #aldous #alruad
 #nerd #donkey #daily
 why are you still reading ? """
+    logOutAfterPost = False
 
-    # WebDriver'ı başlat
+    # Initialize WebDriver
     chrome_options = Options()
-    # #chrome_options.add_argument('--headless')
-    # chrome_options.add_argument("start-maximized")
-
-    # uncomment this 2 lines below
+    # Uncomment for headless mode and debugging
+    # chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=650,650")
+    chrome_options.add_argument("--lang=en")
 
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://www.instagram.com/accounts/login")
 
-    # Giriş yapmayı bekleyin
-    try:
-        username_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "username")))
-        password_input = driver.find_element(By.NAME, "password")
+    #FillCookieManual(driver)
 
-        # Kullanıcı adı ve şifre girin
-        username_input.send_keys(username)
-        password_input.send_keys(password)
-        password_input.send_keys(Keys.RETURN)
-        print("Giriş yapılıyor...")
+    print("Checking cookies...")
+    if os.path.exists("cookies.pkl"):
+        print("Cookie data exists.")
+        with open("cookies.pkl", "rb") as file:
+            cookies = pickle.load(file)
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+        driver.refresh()
+    else:
+        try:
+            ClickToButton(driver, 10, "//input[@name='username']")
+            ClickToButton(driver, 10, "//input[@name='password']")
+
+            username_input = driver.find_element(By.NAME, "username")
+            password_input = driver.find_element(By.NAME, "password")
+
+            username_input.send_keys(username)
+            password_input.send_keys(password)
+            password_input.send_keys(Keys.RETURN)
+            print("Logging in...")
+        except Exception as e:
+            print("An error occurred:", e)
+            driver.quit()
+    
+    # FillCookieManual(driver)
+    # driver.quit()
+    # return
+    
+    # Wait for login to complete
+    print("waiting for load screen (3sec)")
+    time.sleep(3)
+
+    print("closing open notifications popup")
+    TryToClickButton(driver,10,"//button[text()='Not Now']")
+
+    # Create a new post
+    try:
+        print("----- Progress started -----")
+        ClickToButton(driver, 10, "/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[4]/div/span/div/a/div/div/div/div")
+        print("Create button clicked...")
+        ClickToButton(driver, 10, "/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div[4]/div/span/div/div/div/div[1]/a[1]")
+        print("Create new post button clicked...")
     except Exception as e:
-        print("Bir hata oluştu:", e)
+        print("Create new post button not found:", e)
         driver.quit()
 
-    # Giriş yaptıktan sonra bekleyin
-    time.sleep(5)
-
-    # Yeni gönderi oluşturmak için
+    # Upload image
     try:
-        ClickToButton(
-            driver, 10, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[4]/div/span/div/a/div")
-        print("Yeni gönderi butonuna tıklandı...")
-
-        ClickToButton(
-            driver, 10, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[4]/div/span/div/div/div/div[1]/a[1]/div[1]")
-        print("Gönderi oluşturma butonuna tıklandı...")
-    except Exception as e:
-        print("Yeni gönderi butonu bulunamadı:", e)
-        driver.quit()
-
-    # Görüntü yükleme
-    try:
-        print("Görüntü yükleniyor...")
+        print("Uploading image...")
         SendKeys(driver, 10, image_path, "//input[@type='file']")
-        print("Görüntü yükleme başarılı.")
+        print("Image upload successful.")
+        TryToClickButton(driver,10,"//button[text()='OK']")
     except Exception as e:
-        print("Görüntü yükleme başarısız:", e)
+        print("Image upload failed:", e)
         driver.quit()
 
     time.sleep(content_uploadSize)
 
     try:
-        ClickToButton(
-            driver, 10, "/html/body/div[6]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div[4]/button")
-        print("Popup işlem gördü.")
+        ClickToButton(driver, 10, "//div[text()='Next']")
+        print("Next button clicked.")
+        ClickToButton(driver, 10, "//div[text()='Next']")
+        print("Filter Next button clicked.")
     except:
         pass
-    # İleri butonuna basmak
-    try:
-        nextButton = "/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div"
-        ClickToButton(
-            driver, 10, nextButton)
-        print("İleri butonuna tıklandı.")
 
-        # time.sleep(5)  # Bir sonraki sayfanın yüklenmesi için bekleyin
-
-        ClickToButton(
-            driver, 15, nextButton)
-        print("İleri butonuna tekrar tıklandı.")
-    except Exception as e:
-        print("İleri butonu bulunamadı:", e)
-        driver.quit()
-
-    # time.sleep(5)
-
-    # Başlık eklemek ve paylaşmak
+    # Add description and share
     try:
         SendKeys(driver, 10, post_description, "//div[@role='textbox']")
-        print("Gönderi açıklaması yazıldı")
+        print("Post description entered")
 
         time.sleep(3)
 
-        ClickToButton(
-            driver, 15, "/html/body/div[6]/div[1]/div/div[3]/div/div/div/div/div/div/div/div[1]/div/div/div/div[3]/div/div")
-        print("Gönderi paylaşma butonuna tıklandı.")
+        ClickToButton(driver, 15, "//div[text()='Share']")
+        print("Post share button clicked.")
     except Exception as e:
-        print("Gönderi paylaşma başarısız:", e)
+        print("Post sharing failed:", e)
         driver.quit()
 
-    # Paylaşımın tamamlanması için bekleyin
-    time.sleep(30+(content_uploadSize*content_uploadSize))
+    # Wait for the post to be completed
+    time.sleep(30 + (content_uploadSize * content_uploadSize))
 
-    # Çıkış yapma
-    try:
-        ClickToButtonNow(
-            driver, "/html/body/div[6]/div[1]/div/div[2]/div")
-        print("Popup işlem gördü")
+    if logOutAfterPost:
+        try:
+            ClickToButton(driver, 10, "//div[@aria-label='Account']")
+            print("Popup processed")
+            ClickToButton(driver, 10, "//div[text()='Settings']")
+            print("Settings button clicked.")
+            ClickToButton(driver, 15, "//button[text()='Log Out']")
+            print("Log out button clicked.")
+        except Exception as e:
+            print("Log out failed:", e)
+            driver.quit()
 
-        # time.sleep(3)
-        ClickToButton(driver, 10,
-                      "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[6]/div/span/div/a")
-        print("Profil butonuna tıklandı.")
-
-        # time.sleep(3)
-
-        ClickToButton(
-            driver, 10, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[2]/div/div[1]/div[2]/div")
-        print("Ayarlar butonuna tıklandı.")
-
-        # time.sleep(3)
-
-        ClickToButton(driver, 15,
-                      "/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/button[8]")
-        print("Çıkış butonuna tıklandı.")
-    except Exception as e:
-        print("Çıkış yapma başarısız:", e)
-        driver.quit()
-
-    # Tarayıcıyı kapatın
+    # Close the browser
+    SaveCookies(driver)
     time.sleep(5)
     driver.quit()
 
-    print("Upload tamamlandı")
-
+    print("Upload completed")
 
 if __name__ == "__main__":
     Run()
